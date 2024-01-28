cc1554f32ebaca115c6e282e19f961af2b7e6467
Mehdi Makni <mmakni@mit.edu>
2024-01-27 21:08:56 -0500

    Test adaptive mask.

--- src/tests/scripts/supercloud_test_adaptive_mask.sh
+++ src/tests/scripts/supercloud_test_adaptive_mask.sh
@@ -2,9 +2,11 @@
 #SBATCH -c 20
 #SBATCH -t 2-00:0 #Request runtime of 2 days
 #SBATCH --gres=gpu:volta:1
-#SBATCH -o ../nb2_test_adaptive_mask/output_logs/output_run_%A_%a.txt
-#SBATCH -e ../nb2_test_adaptive_mask/error_logs/error_run_%A_%a.txt
+#SBATCH -o ../test_adaptive_mask_state/output_logs/output_run_%A_%a.txt
+#SBATCH -e ../test_adaptive_mask_state/error_logs/error_run_%A_%a.txt
 #SBATCH --array=0
+
+module load anaconda/2023a
 source activate pruning
 
 cd ..
