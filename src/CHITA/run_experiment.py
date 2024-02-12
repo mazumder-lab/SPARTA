265e249bdccc8758a60dcdb7cfb95d0b3d662703
Mehdi Makni <mmakni@mit.edu>
2024-02-12 17:41:52 -0500

    Wip.

--- src/CHITA/run_experiment.py
+++ src/CHITA/run_experiment.py
@@ -252,7 +252,7 @@ for (
         }
     )
     model_to_save = multi_stage_pruner.pruner.model
-    torch.save(model_to_save.state_dict(), f"chita_model_{int(sparsity * 100)}.pth")
+    torch.save(model_to_save.state_dict(), f"chita_model_{int(args.sparsity * 100)}_{l2}.pth")
     acc_different_methods[-1]["results"] = multi_stage_pruner.results
     with open(FILE, "w") as file:
         json.dump(acc_different_methods, file, cls=NpEncoder)
