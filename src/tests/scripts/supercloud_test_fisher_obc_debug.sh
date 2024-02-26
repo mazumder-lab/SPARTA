9b2fd6cf450e2c933b45084ba1661be9cfbc54bc
Mehdi <mmakni@mit.edu>
2024-02-26 01:52:52 -0500

    Wip.

--- src/tests/scripts/supercloud_test_fisher_obc_debug.sh
+++ src/tests/scripts/supercloud_test_fisher_obc_debug.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
-#SBATCH -c 20
+#SBATCH -c 40
 #SBATCH -t 2-00:0 #Request runtime of 2 days
 #SBATCH --gres=gpu:volta:2
 #SBATCH --mem=340G
 #SBATCH --exclusive
 #SBATCH -o ../test_fisher_obc_mask_debug/output_logs/output_run_%A_%a.txt
 #SBATCH -e ../test_fisher_obc_mask_debug/error_logs/error_run_%A_%a.txt
-#SBATCH --array=0-3
+#SBATCH --array=0-23
 
 TASK_ID=$SLURM_ARRAY_TASK_ID
 echo $TASK_ID
@@ -32,25 +32,19 @@ classifier_lr=${classifier_lrs[$(($TASK_ID % 1))]}
 lr=${lrs[$(($TASK_ID % 1))]}
 TASK_ID=$((TASK_ID/1))
 
-sparsities=(0.8) 
-sparsity=${sparsities[$(($TASK_ID % 1))]}
-TASK_ID=$((TASK_ID/1))
+sparsities=(0.5 0.1) 
+sparsity=${sparsities[$(($TASK_ID % 2))]}
+TASK_ID=$((TASK_ID/2))
 
 epochss=(50 35) 
 epochs=${epochss[$(($TASK_ID % 2))]}
 TASK_ID=$((TASK_ID/2))
 
-use_w_tildes=(True True)
-correction_coefficients=(1.0 0.01)
-use_w_tilde=${use_w_tildes[$(($TASK_ID % 2))]}
-correction_coefficient=${correction_coefficients[$(($TASK_ID % 2))]}
-TASK_ID=$((TASK_ID/2))
-
-# use_w_tildes=(False)
-# correction_coefficients=(0.0)
-# use_w_tilde=${use_w_tildes[$(($TASK_ID % 1))]}
-# correction_coefficient=${correction_coefficients[$(($TASK_ID % 1))]}
-# TASK_ID=$((TASK_ID/1))
+use_w_tildes=(True True False)
+correction_coefficients=(1.0 0.01 0.0)
+use_w_tilde=${use_w_tildes[$(($TASK_ID % 3))]}
+correction_coefficient=${correction_coefficients[$(($TASK_ID % 3))]}
+TASK_ID=$((TASK_ID/3))
 
 use_fisher_mask_with_true_gradss=(True False)
 use_fisher_mask_with_true_grads=${use_fisher_mask_with_true_gradss[$(($TASK_ID % 2))]}
