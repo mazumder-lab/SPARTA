360005cd134d3040b229705c35d692bf7a1ab7a1
Mehdi Makni <mmakni@mit.edu>
2024-09-13 12:22:24 -0400

    Wip.

--- src/scripts/to_benchmark_deit_tiny_all_methods.py
+++ src/scripts/to_benchmark_deit_tiny_all_methods.py
@@ -22,7 +22,6 @@ l_epochs = [50]
 l_lrs = [(0.1, 0.005), (0.1, 0.001), (0.01, 0.01), (0.005, 0.005)]
 l_sparisities = [0.2]
 l_use_fixed_w_mask_findings = [1]
-l_use_cosine_more_epochs = [1]
 l_models = ["deit_tiny_patch16_224"]
 l_name_datasets = ["cifar10", "cifar100", "OrganAMNIST"]
 l_epoch_mask_finding = [5]
@@ -33,7 +32,6 @@ l_seeds = [0, 42, 2024, 256, 16]
 l_scripts_to_run = []
 for cnt, (
     use_fixed_w_mask_finding,
-    use_cosine_more_epochs,
     model,
     epsilon,
     clipping,
@@ -44,7 +42,6 @@ for cnt, (
 ) in enumerate(
     product(
         l_use_fixed_w_mask_findings,
-        l_use_cosine_more_epochs,
         l_models,
         l_epsilons,
         l_clippings,
@@ -62,7 +59,7 @@ for cnt, (
             command = f'python train_cifar.py --method_name "mp_weights" --max_physical_batch_size 100 --epoch_mask_finding -1 --dataset {name_dataset} --batch_size {batch_size} --model {model} --num_classes {num_classes} --classifier_lr {classifier_lr} --lr {lr} --lsr 0.0 --wd 0.0 --momentum 0.9 --lr_schedule_type "onecycle" --num_epochs {epochs} --warm_up 0.02 --use_gn True --sparsity {sparsity} --epsilon {epsilon} --delta 1e-5 --clipping {clipping} --experiment_dir {EXPERIMENT_DIR} --seed {seed} --TASK_ID {cnt}'
             l_scripts_to_run.append(command)
             for epoch_mask_finding in l_epoch_mask_finding:
-                command = f'python train_cifar.py --use_last_layer_only_init {use_last_layer_only_init} --method_name "random_masking"               --max_physical_batch_size 100 --dataset {name_dataset} --batch_size {batch_size} --model {model} --num_classes {num_classes} --classifier_lr {classifier_lr} --lr {lr} --lsr 0.0 --wd 0.0 --momentum 0.9 --lr_schedule_type "onecycle" --num_epochs {epochs} --warm_up 0.02 --use_gn True --sparsity {sparsity} --epsilon {epsilon} --delta 1e-5 --clipping {clipping} --experiment_dir {EXPERIMENT_DIR} --seed {seed} --TASK_ID {cnt}'
+                command = f'python train_cifar.py --use_last_layer_only_init {use_last_layer_only_init} --method_name "row_pruning_noisy_grads" --max_physical_batch_size 100 --epoch_mask_finding {epoch_mask_finding} --dataset {name_dataset} --batch_size {batch_size} --model {model} --num_classes {num_classes} --classifier_lr {classifier_lr} --lr {lr} --lsr 0.0 --wd 0.0 --momentum 0.9 --lr_schedule_type "onecycle" --num_epochs {epochs} --warm_up 0.02 --use_gn True --sparsity {sparsity} --epsilon {epsilon} --delta 1e-5 --clipping {clipping} --experiment_dir {EXPERIMENT_DIR} --seed {seed} --TASK_ID {cnt}'
                 l_scripts_to_run.append(command)
 
 
